210297544554deeb3727146addc1aeeaca971070
Leonardo Romeiro <leonardo.romeiro@procempa.com.br>
2020-09-02 16:47:15 +0000

    fix bad translation from ansi to html

--- src/utils/console-to-html.js
+++ src/utils/console-to-html.js
@@ -1,8 +1,20 @@
-import Convert from 'ansi-to-html'
-const convert = new Convert();
- 
+export function _replaceAllColor(text) {
+  return text
+    .replace(/\[0m/g, '</span>')
+    .replace(/\[39m/g, '</span>')
+    .replace(/\[30m/g, '<span style="color:gray">')
+    .replace(/\[31m/g, '<span style="color:red">')
+    .replace(/\[32m/g, '<span style="color:green">')
+    .replace(/\[33m/g, '<span style="color:yellow">')
+    .replace(/\[34m/g, '<span style="color:blue">')
+    .replace(/\[35m/g, '<span style="color:magenta">')
+    .replace(/\[36m/g, '<span style="color:cyan">')
+    .replace(/\[37m/g, '<span style="color:gray">')
+    .replace(/\[90m/g, '<span style="color:gray">')
+}
+
 export function consoleToHtmlLine(text = '') {
-  return convert.toHtml(text)
+  return _replaceAllColor(text)
 }
 
 export function consoleToHtml(text) {
